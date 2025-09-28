0ca2c2cc226316d2da7fbd2ae5f51fc3d9daf565
alexgulliver <alex.gulliver6@gmail.com>
2025-09-28 21:04:33 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("scripts");
+    eleventyConfig.addPassthroughCopy("photos");
+  };
