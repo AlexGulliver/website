ca5c16889fb35e430767f461a9e6813fdd66028b
alexgulliver <alex.gulliver6@gmail.com>
2025-09-28 21:08:53 +0100

    reconfigure for vercel

--- .eleventy.js
+++ .eleventy.js
@@ -5,3 +5,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("scripts");
     eleventyConfig.addPassthroughCopy("photos");
   };
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
