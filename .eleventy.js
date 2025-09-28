8117069a6d0d21faa7c7b4e33e18d440c0f43cc2
alexgulliver <alex.gulliver6@gmail.com>
2025-09-28 21:11:19 +0100

    reconfigure for vercel attempt 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("styles");
-    eleventyConfig.addPassthroughCopy("fonts");
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("scripts");
-    eleventyConfig.addPassthroughCopy("photos");
-  };
+  eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("scripts");
+  eleventyConfig.addPassthroughCopy("photos");
 
   return {
     dir: {
-      input: "src",
-      output: "public"
+      input: "src",    // where your content lives
+      output: "public" // Vercel expects this as output
     }
   };
+};
